2ef50e1552cc2ecd45853450993da1d6c2c41823
RaulMesoneroFernandezz <60215730+RaulMesoneroFernandezz@users.noreply.github.com>
2021-07-05 23:38:02 +0200

    Add files via upload

---
+++ cuenta_atras.asm
@@ -0,0 +1,86 @@
+	.globl cuenta_atras
+	
+	.include "servicios.asm"
+	
+	.data
+
+msg1:	.string "Introduce el numero en el que se empieza: "
+msg2:	.string "Introduce el numero en que se termina (no incluido): "
+msg3:	.string "El primer numero debe de ser menor que el segundo.\n"
+
+	.text
+	
+cuenta_atras:
+
+	li t2, DISPLAY_R
+	li t3, DISPLAY_L
+
+	#-- Pedir al usuario en que numero quiere comenzar a contar
+	la a0, msg1
+	li a7, PRINT_STRING
+	ecall
+	
+	#-- Leer primer entero (t0)
+	li a7, READ_INT
+	ecall
+	mv t0, a0
+	
+	#-- Pedir al usuario hasta que numero quiere contar
+	la a0, msg2
+	li a7, PRINT_STRING
+	ecall
+	
+	#-- Leer segundo entero (t1)
+	li a7, READ_INT
+	ecall
+	mv t1, a0
+	
+	#-- Comprobar si el primer numero es menor que el segundo
+	blt t0, t1, error
+	
+	#-- Crear la pila mínima: 20 bytes
+	addi sp, sp, -20
+	
+	#---- GUARDAR RA EN LA PILA
+	sw ra, 16(sp)
+
+	b contar
+
+contar:
+
+	mv a0, t0  #-- El primer parametro de la funcion
+	mv a1, t2  #-- El segundo parametro de la funcion
+	
+	sw t0, 12(sp) #-- Guardamos el valor de t0
+	sw t1, 8(sp) #-- Guardamos el valor de t1
+	sw t2, 4(sp) #-- Guardamos el valor de t2
+	sw t3, 0(sp) #-- Guardamos el valor de t3
+	
+	jal colocar_display
+	
+	lw t0, 12(sp) #-- Recuperamos de la pila el valor de t0
+	lw t1, 8(sp) #-- Recuperamos de la pila el valor de t1
+	lw t2, 4(sp) #-- Recuperamos de la pila el valor de t1
+	lw t3, 0(sp) #-- Recuperamos de la pila el valor de t1
+
+	addi t0, t0, -1 #-- Pasamos al siguiente numero
+	
+	beq t0, t1, end #-- Comprobamos si ya se ha llegado al maximo
+	
+	b contar #-- Repetimos
+
+error:
+
+	la a0, msg3
+	li a7, PRINT_STRING
+	ecall
+	
+	b end
+
+end:
+	#-- Cargamos la direccion de retorno
+	lw ra, 16(sp)
+	#-- Liberar el espacio de la pila
+	addi sp, sp, 20
+	#-- Terminar
+	ret
